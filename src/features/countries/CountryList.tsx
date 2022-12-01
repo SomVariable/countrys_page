ab6f47859d8b5b865a51d5e8a2daa93e3b8c5016
SomVariable <79557545+SomVariable@users.noreply.github.com>
2022-12-01 12:00:19 +0200

    reformatted theme features to tsx

--- src/features/countries/CountryList.tsx
+++ src/features/countries/CountryList.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { List } from '../../components/List';
 import { Card } from '../../components/Card';
 import { useCountries } from './use-countries';
-
+import {CountryInfo} from "types"
 
 const CountryList = () => {
   const navigate = useNavigate();
@@ -18,7 +18,7 @@ const CountryList = () => {
       {status === 'received' && (
       <List>
         {countries.map((c) => {
-          const countryInfo = {
+          const countryInfo: CountryInfo = {
             img: c.flags.png,
             name: c.name,
             info: [
@@ -40,7 +40,7 @@ const CountryList = () => {
           return (
             <Card
               key={c.name}
-              onClick={() => navigate(`/country/${c.name}`)}
+              onClick={() => void navigate(`/country/${c.name}`)}
               {...countryInfo}
             />
           );
